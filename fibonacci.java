4794c9c84a2b32b226502d9295ab65a532f3c228
MizginElcicek <121758869+MizginElcicek@users.noreply.github.com>
2023-03-26 21:23:57 +0300

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,16 @@
+package Kodluyoruz;
+
+public class fibonacci {
+    //Recursive Metot ile Fibo
+     static int fib(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+
+    }
+}
