41474bfaf9c99b68deea766f5929e94a36bd8a2f
MizginElcicek <121758869+MizginElcicek@users.noreply.github.com>
2023-04-27 20:36:47 +0300

    Update Calisan.java

--- Calisan.java
+++ Calisan.java
@@ -11,7 +11,7 @@ public class Calisan {
     double maasA;
     double toplamM;
 
-    Calisan(String ad,String soyad,int maas,int calSaat,double tarih,double bonus){
+    Calisan(String ad,String soyad,int maas,int calSaat,double tarih,double bonus){// kullanıcı vergiye aldığı maaşı ve 40 saatten fazla çalıştı ise kaç saat çalıştığını girmeli
         this.ad = ad;
         this.soyad = soyad;
         this.maas = maas;
